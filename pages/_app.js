2402df998642641216412e12023ced72e311de4e
Manas Tripathi <TRIPATHIMANAS98@GMAIL.COM>
2023-01-08 15:13:04 +0530

    added context and initialised page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-import '../styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { createContext, useState } from "react";
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+export const SpaceContext = createContext({});
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  const [spaceData, setSpaceData] = useState({
+    switchState: "rockets",
+    availableRockets: null,
+    availableCapsules: null,
+    selectedRocketDetails: null,
+    selectedCapsuleDetails: null,
+    rocketSearchFilters: null,
+    capsuleSearchFilters: null,
+    rocketsFilterData: null,
+    capsulesFilterData: null,
+  });
+  const contextSetter = (stateToUpdate) => {
+    setSpaceData({ ...spaceData, ...stateToUpdate });
+  };
+  return (
+    <SessionProvider session={session}>
+      <SpaceContext.Provider value={{ state: spaceData, contextSetter }}>
+        <Component {...pageProps} />
+      </SpaceContext.Provider>
+    </SessionProvider>
+  );
 }
