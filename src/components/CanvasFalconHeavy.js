dd0724f3d952b8e38904fe0c3ed4742d6c53ceeb
Manas Tripathi <TRIPATHIMANAS98@GMAIL.COM>
2023-01-08 15:13:51 +0530

    added 3D Falcon heavy model using threejs

---
+++ src/components/CanvasFalconHeavy.js
@@ -0,0 +1,43 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import styles from "../../styles/components/CanvasFalconHeavy.module.css";
+import { Model } from "./Model";
+function CanvasFalconHeavy() {
+  return (
+    <Canvas className={styles.wrapper}>
+      <mesh rotation={[0, 90, 0]}>
+        <ambientLight intensity={3} color="#ffffff" />
+        <directionalLight
+          position={[-2, 5, 2]}
+          intensity={1.5}
+          color="#ffffed"
+        />
+        {/* <directionalLight
+          position={[-2, 5, 2]}
+          intensity={1.5}
+          color="#ffffed"
+        /> */}
+        <directionalLight
+          position={[2, 5, -2]}
+          intensity={1.5}
+          color="#99edee"
+        />
+        {/* <boxBufferGeometry attach="geometry" args={[3, 3, 3]} /> */}
+        <Model />
+        <meshLambertMaterial attach="material" color="#ffffff" />
+        <OrbitControls
+          autoRotate={true}
+          enableZoom={false}
+          enableRotate={true}
+          autoRotateSpeed={5}
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
+          //   rotation={[0, 0, 0]}
+        />
+      </mesh>
+    </Canvas>
+  );
+}
+
+export default CanvasFalconHeavy;
